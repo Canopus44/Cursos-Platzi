83049ce48c936c56d9599889ce2a836a49bf85b3
Santiago Molina Molina <canopus44@gmail.com>
2023-01-09 18:07:34 -0500

    Clase 20 - Curso practico javascript Carrito de compras dinamico

--- personalProjects/frontend-developer-1/main.js
+++ personalProjects/frontend-developer-1/main.js
@@ -1,15 +1,40 @@
+// Menu for desktop
 const navbarEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+navbarEmail.addEventListener("click", toggleDesktopMenu);
+
+// Menu for mobile
 const burgerMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-
-navbarEmail.addEventListener("click", toggleDesktopMenu);
 burgerMenu.addEventListener("click", toggleMobileMenu);
 
+// Shoping cart
+const navbarShoppingCart = document.querySelector(".navbar-shopping-cart");
+const shoppingCart = document.querySelector(".product-detail");
+navbarShoppingCart.addEventListener("click", toggleShoppingCart);
+
+// Functions
 function toggleDesktopMenu() {
+  const isShoppingCartClosed = shoppingCart.classList.contains("inactive");
+  if (!isShoppingCartClosed) {
+    toggleShoppingCart();
+  }
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
+  const isShoppingCartClosed = shoppingCart.classList.contains("inactive");
+  if (!isShoppingCartClosed) {
+    toggleShoppingCart();
+  }
   mobileMenu.classList.toggle("inactive");
+}
+
+function toggleShoppingCart() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    toggleMobileMenu();
+  }
+  shoppingCart.classList.toggle("inactive");
 }
