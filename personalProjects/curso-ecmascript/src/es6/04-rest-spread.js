2bbd9bb6fbc60ff96c1620a7dce9065e4cb7cbb6
Santiago Molina Molina <canopus44@gmail.com>
2023-01-09 22:21:12 -0500

    Clase 7 - Curso ECMAScript ES6: Asignacion de destructuracion

---
+++ personalProjects/curso-ecmascript/src/es6/04-rest-spread.js
@@ -0,0 +1,11 @@
+// Arrays destructuring
+
+let fruits = ["Apple", "Banana"];
+let [a, b] = fruits;
+console.log(a, b);
+
+// Objects destructuring
+
+let user = { username: "Santiago", age: 28 };
+let { username, age } = user;
+console.log(username, user.age);
