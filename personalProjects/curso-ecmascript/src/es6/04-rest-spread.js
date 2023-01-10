394801fd38963139c28976671fa681cd04e468bd
Santiago Molina Molina <canopus44@gmail.com>
2023-01-09 22:51:45 -0500

    Clase 8 - Curso ECMAScript ES6: spread operator

--- personalProjects/curso-ecmascript/src/es6/04-rest-spread.js
+++ personalProjects/curso-ecmascript/src/es6/04-rest-spread.js
@@ -9,3 +9,21 @@ console.log(a, b);
 let user = { username: "Santiago", age: 28 };
 let { username, age } = user;
 console.log(username, user.age);
+
+// Spread operator
+
+let person = { name: "Santiago", age: 28 };
+let country = "CO";
+
+let data = { id: 1, ...person, country };
+console.log(data);
+
+// rest
+
+function sum(num, ...values) {
+  console.log(values);
+  console.log(num + values[0]);
+  return num + values[0];
+}
+
+sum(1, 1, 2, 3);
